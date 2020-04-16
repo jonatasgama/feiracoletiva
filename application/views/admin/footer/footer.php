013929a0d78b61206baa74844bf1b6f3f14deaa8
jonatas-gama <jonatasgama@rssolucoesconsultoria.com>
2020-04-16 14:51:17 -0300

    removido jquery mask por incompatibilidade

--- application/views/admin/footer/footer.php
+++ application/views/admin/footer/footer.php
@@ -35,8 +35,6 @@
   <!-- Page level custom scripts -->
   <script src="<?=base_url('admin_assets/js/demo/chart-area-demo.js');?>"></script>
   <script src="<?=base_url('admin_assets/js/demo/chart-pie-demo.js');?>"></script>
-  
-  <<!--script src="<?=base_url('admin_assets/js/jquery.mask.min.js');?>"></script>-->
 
 
 
